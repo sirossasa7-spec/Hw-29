174bd21d383ea5b540d5dea1a4d1a69d7742679c
sirossasa7@gmail.com <sirossasa7@gmail.com>
2026-05-17 16:14:55 +0200

    Colleague.cpp

--- Hw-29/Colleague.cpp
+++ Hw-29/Colleague.cpp
@@ -1,1 +1,16 @@
 #include "Colleague.h"
+
+Colleague::Colleague(string org, string addr, string ph, string person) {
+    organization = org;
+    address = addr;
+    phone = ph;
+    contactPerson = person;
+}
+
+void Colleague::show() const {
+    cout << "\n[Colleague]\n";
+    cout << "Organization: " << organization << endl;
+    cout << "Address: " << address << endl;
+    cout << "Phone: " << phone << endl;
+    cout << "Contact person: " << contactPerson << endl;
+}
