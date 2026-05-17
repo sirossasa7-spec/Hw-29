706cc270c7643bd2db5bae987fccf10d32e8523a
sirossasa7@gmail.com <sirossasa7@gmail.com>
2026-05-17 16:13:13 +0200

    Person.cpp

--- Hw-29/Person.cpp
+++ Hw-29/Person.cpp
@@ -1,1 +1,14 @@
 #include "Person.h"
+
+Person::Person(string s, string a, string p) {
+    surname = s;
+    address = a;
+    phone = p;
+}
+
+void Person::show() const {
+    cout << "\n[Person]\n";
+    cout << "Surname: " << surname << endl;
+    cout << "Address: " << address << endl;
+    cout << "Phone: " << phone << endl;
+}
